cadd791595021f9604817c9e4fac0acedf6473df
Brian <clockworkai@outlook.com>
2023-07-28 15:32:18 +0100

    landing page layout tweek

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Typography, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import { Box, Typography, Select, MenuItem, FormControl, InputLabel, Grid } from '@mui/material';
 
 const AppLandingPage = () => {
     const [location, setLocation] = useState('');
@@ -38,43 +38,48 @@ const AppLandingPage = () => {
             >
                 Welcome to the Super Inventory System
             </Typography>
-
-            <Box
-                style={{
-                    marginTop: '50px',
-                    backgroundColor: 'white',
-                    padding: '20px',
-                    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-                    borderRadius: '5px'
-                }}
-            >
-                <FormControl fullWidth style={{ marginBottom: '20px' }}>
-                    <InputLabel id="location-label">Location</InputLabel>
-                    <Select
-                        labelId="location-label"
-                        id="location-select"
-                        value={location}
-                        onChange={handleLocationChange}
+            <Grid container justifyContent="center" style={{ marginTop: '50px' }}>
+                <Grid item xs={11} sm={8} md={6} lg={4}>
+                    <Box
+                        style={{
+                            marginTop: '50px',
+                            backgroundColor: 'white',
+                            padding: '20px',
+                            boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                            borderRadius: '5px'
+                        }}
                     >
-                        <MenuItem value={'location1'}>Location 1</MenuItem>
-                        <MenuItem value={'location2'}>Location 2</MenuItem>
-                    </Select>
-                </FormControl>
+                        <FormControl fullWidth style={{ marginBottom: '20px' }}>
+                            <InputLabel id="location-label">Location</InputLabel>
+                            <Select
+                                labelId="location-label"
+                                id="location-select"
+                                value={location}
+                                onChange={handleLocationChange}
+                                label="location"
+                            >
+                                <MenuItem value={'location1'}>Location 1</MenuItem>
+                                <MenuItem value={'location2'}>Location 2</MenuItem>
+                            </Select>
+                        </FormControl>
 
-                <FormControl fullWidth>
-                    <InputLabel id="staff-member-label">Staff Member</InputLabel>
-                    <Select
-                        labelId="staff-member-label"
-                        id="staff-member-select"
-                        value={staffMember}
-                        onChange={handleStaffMemberChange}
-                        disabled={isStaffMemberDisabled}
-                    >
-                        <MenuItem value={'staff1'}>Staff Member 1</MenuItem>
-                        <MenuItem value={'staff2'}>Staff Member 2</MenuItem>
-                    </Select>
-                </FormControl>
-            </Box>
+                        <FormControl fullWidth>
+                            <InputLabel id="staff-member-label">Staff</InputLabel>
+                            <Select
+                                labelId="staff-member-label"
+                                id="staff-member-select"
+                                value={staffMember}
+                                onChange={handleStaffMemberChange}
+                                disabled={isStaffMemberDisabled}
+                                label="member"
+                            >
+                                <MenuItem value={'staff1'}>Staff Member 1</MenuItem>
+                                <MenuItem value={'staff2'}>Staff Member 2</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Grid>
+            </Grid>
         </Box>
     );
 };
