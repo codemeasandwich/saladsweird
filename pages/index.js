3059aa9dc11c183ec7916d22990dfab52764b6df
Brian <clockworkai@outlook.com>
2023-07-28 15:20:36 +0100

    Placeholder for Location & Staff Selection

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Typography, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 
 const AppLandingPage = () => {
+    const [location, setLocation] = useState('');
+    const [staffMember, setStaffMember] = useState('');
+    const [isStaffMemberDisabled, setStaffMemberDisabled] = useState(true);
+
+    const handleLocationChange = (event) => {
+        setLocation(event.target.value);
+        // Enable staff member drop down if a location is selected
+        setStaffMemberDisabled(event.target.value === '' ? true : false);
+    };
+
+    const handleStaffMemberChange = (event) => {
+        setStaffMember(event.target.value);
+    };
+
     return (
         <Box
             style={{
                 minHeight: '100vh',
                 backgroundImage: 'linear-gradient(to right, #ff5e62, #ff9966)',
                 display: 'flex',
+                flexDirection: 'column',
                 alignItems: 'center',
                 justifyContent: 'center',
                 textAlign: 'center'
@@ -23,6 +38,43 @@ const AppLandingPage = () => {
             >
                 Welcome to the Super Inventory System
             </Typography>
+
+            <Box
+                style={{
+                    marginTop: '50px',
+                    backgroundColor: 'white',
+                    padding: '20px',
+                    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                    borderRadius: '5px'
+                }}
+            >
+                <FormControl fullWidth style={{ marginBottom: '20px' }}>
+                    <InputLabel id="location-label">Location</InputLabel>
+                    <Select
+                        labelId="location-label"
+                        id="location-select"
+                        value={location}
+                        onChange={handleLocationChange}
+                    >
+                        <MenuItem value={'location1'}>Location 1</MenuItem>
+                        <MenuItem value={'location2'}>Location 2</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <FormControl fullWidth>
+                    <InputLabel id="staff-member-label">Staff Member</InputLabel>
+                    <Select
+                        labelId="staff-member-label"
+                        id="staff-member-select"
+                        value={staffMember}
+                        onChange={handleStaffMemberChange}
+                        disabled={isStaffMemberDisabled}
+                    >
+                        <MenuItem value={'staff1'}>Staff Member 1</MenuItem>
+                        <MenuItem value={'staff2'}>Staff Member 2</MenuItem>
+                    </Select>
+                </FormControl>
+            </Box>
         </Box>
     );
 };
